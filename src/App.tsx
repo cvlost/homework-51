21246a6a1d0e00a245db303410bcd4f1392f4f0f
Pavlov Vladimir <connection.lost.again@gmail.com>
2022-10-21 08:41:50 +0600

    App component: added logic to produce random set

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Ball from "./Ball/Ball";
 
 function App() {
+  const getRandFromTo = (a: number, b: number) => Math.floor((Math.random() * (b - a + 1)) + a);
+
+  const getNumbers = () => {
+    const numbers: number[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      let rand = getRandFromTo(5, 36);
+      while (numbers.indexOf(rand) >= 0) rand = getRandFromTo(5, 36);
+      numbers.push(rand);
+    }
+
+    numbers.sort((a, b) => a - b);
+    return numbers;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {getNumbers().map(number => <Ball content={number} key={number} />)}
     </div>
   );
 }
